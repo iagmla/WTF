4cefedfca1cf82b1176e208f337f99d95345707d
Karl Zander <karl.c.zander@gmail.com>
2024-08-12 16:11:10 -0700

    v0.0.2

--- src/formatter.c
+++ src/formatter.c
@@ -8,6 +8,11 @@ void format_text(char *infilename, char *outfilename, int grouplen, int groups_p
     int letters_per_line = grouplen * groups_per_line;
     int blocks = datalen / letters_per_line;
     int extra = datalen % letters_per_line;
+    //if (extra != 0) {
+    //    blocks += 1;
+    //}
+    int lastgroups = extra / grouplen;
+    int lastextra = extra % grouplen;
     uint8_t block[grouplen];
     uint8_t space = 32;
     uint8_t newline = 10;
@@ -20,6 +25,21 @@ void format_text(char *infilename, char *outfilename, int grouplen, int groups_p
             }
         }
         fwrite(&newline, 1, 1, outfile);
+        if (i == (blocks - 1)) {
+            for (int l = 0; l < lastgroups; l++) {
+                fread(block, 1, grouplen, infile);
+                fwrite(block, 1, grouplen, outfile);
+                if (l != (groups_per_line - 1)) {
+                    fwrite(&space, 1, 1, outfile);
+                }
+                else {
+                    if (lastextra != 0) {
+                        fread(block, 1, lastextra, infile);
+                        fwrite(block, 1, lastextra, outfile);
+                    }
+                }
+            }
+        }
     }
     fclose(infile);
     fclose(outfile);
--- src/wtf.c
+++ src/wtf.c
@@ -5,7 +5,7 @@
 
 /* War Time Formatter */
 /* by KryptoMagick (Karl Zander) */
-/* v0.0.1 */
+/* v0.0.2 */
 
 void usage() {
     printf("War Time Formatter usage:\n");
